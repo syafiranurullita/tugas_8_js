3ab714a1a6a8ca0da5ef93516569eb559ecbaa0b
syafiranurullita <syafirnurullita3107@gmail.com>
2022-07-18 19:00:38 +0700

    tugas 8 js
    untuk Niomic

---
+++ Tugas8/Tugas8.js
@@ -0,0 +1,12 @@
+function panggilObjects(){
+  var laptop = {
+    type: 'Macbook',
+    harga: 10000000,
+    warna: 'putih',
+  }
+  laptop.type = 'ASUS';
+  console.log(laptop)
+  console.log(laptop.type)
+}
+
+panggilObjects()
